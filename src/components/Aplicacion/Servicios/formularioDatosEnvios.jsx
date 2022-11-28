1914c9a6dfecbd438c0c6ee798904fd45380c084
adferatuz <viadfeur@gmail.com>
2022-11-27 23:14:08 -0500

    formulario datos envios

--- src/components/Aplicacion/Servicios/formularioDatosEnvios.jsx
+++ src/components/Aplicacion/Servicios/formularioDatosEnvios.jsx
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 const FormDatosEnvios = ({vista, handleClickChild}) => {
     useEffect(() =>{         
     },[vista])
+
+    const {register, formState: {errors}, handleSubmit} = useForm ();
+    const customSubmit = (data) =>{console.log(data)};
     
     const handleClick = () => {
         handleClickChild();
@@ -13,28 +17,66 @@ const FormDatosEnvios = ({vista, handleClickChild}) => {
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title">Modal title</h5>
+                        <h5 className="modal-title">Registro envios</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleClick}></button>
                     </div>
                 <div className="modal-body">
-                    <form action="">
-                        <div className="row">
+                    <form action="" onSubmit={handleSubmit(customSubmit)}>
+                        <div className="row form-envio__datos">
                         <div className="col-4">
                             <h6 id="margen">Digite fecha y hora para atender al personal del envio</h6>
-                            <label htmlFor="itime"><h6>Tiempo</h6></label>
-                            <input id="itime" type="time" className="form-control"   aria-describedby="addon-wrapping"/>
-                            <label htmlFor="idate"><h6>Fecha</h6></label>
-                            <input id="idate" className="form-control" type="date" min="2021-01-01" max="2023-12-31" />                        
+                            <label htmlFor="time"><h6>Tiempo</h6></label>
+                            <input {...register("time", { required: true })}
+                            aria-invalid = {errors.time ? "true": "false"}
+                            id="time"
+                            name="time" 
+                            type="time" 
+                            className="form-control"   
+                            aria-describedby="addon-wrapping"/>
+                            {errors.time && <span>This field is required.<br /></span>}
+
+                            <label htmlFor="date"><h6>Fecha</h6></label>
+                            <input {...register("date", { required: true })}
+                            aria-invalid = {errors.date ? "true": "false"} 
+                            id="date" 
+                            className="form-control" 
+                            type="date" 
+                            min="2021-01-01" 
+                            max="2023-12-31" 
+                            name="date"/>
+                            {errors.date && <span>This field is required.<br /></span>}
+
                         </div>               
-                        <div className="col-2"><h6>Datos del envio en cm</h6>
-                            <label htmlFor="ialto"><h6>Alto</h6></label>
-                            <input type="text" className="form-control" id="ialto" />
-                            <label htmlFor="iancho"><h6>Ancho</h6></label>
-                            <input type="text" className="form-control" id="iancho" />
-                            <label htmlFor="ilargo"><h6>Largo</h6></label>
-                            <input type="text" className="form-control" id="ilargo" />
+                        <div className="col-4"><h6>Datos del envio en cm</h6>
+                            <label htmlFor="alto"><h6>Alto</h6></label>
+                            <input {...register("alto", { required: true })}
+                            aria-invalid = {errors.alto ? "true": "false"}
+                            type="number" 
+                            className="form-control" 
+                            id="alto"
+                            name="alto" />
+                            {errors.alto && <span>This field is required.<br /></span>}
+
+                            <label htmlFor="ancho"><h6>Ancho</h6></label>
+                            <input {...register("ancho", { required: true })}
+                            aria-invalid = {errors.ancho ? "true": "false"}
+                            type="number" 
+                            className="form-control" 
+                            id="ancho"
+                            name="ancho" />
+                            {errors.ancho && <span>This field is required.<br /></span>}
+
+                            <label htmlFor="largo"><h6>Largo</h6></label>
+                            <input {...register("largo", { required: true })}
+                            aria-invalid = {errors.largo? "true": "false"}
+                            type="number" 
+                            className="form-control" 
+                            id="largo" 
+                            name="largo"/>
+                            {errors.largo && <span>This field is required.<br /></span>}
+
                         </div>
-                        <div className="col-2"><h6>Mercancia delicada?</h6>                   
+                        <div className="col-4"><h6>Mercancia delicada?</h6>                   
                             <div className="input-group mb-3">                   
                                 <select className="form-select" id="inputGroupSelect01">
                                     <option select ="true">Elige</option>
@@ -42,15 +84,22 @@ const FormDatosEnvios = ({vista, handleClickChild}) => {
                                     <option defaultValue="2">No</option>   
                                 </select>
                             </div>
-                            <label htmlFor="ipeso"><h6>Peso aproximado en kg</h6></label>
-                            <input type="text" className="form-control" id="ipeso" />   
+                            <label htmlFor="peso"><h6>Peso aproximado en kg</h6></label>
+                            <input {...register("peso", { required: true })}
+                            aria-invalid = {errors.peso ? "true": "false"}
+                            type="number" 
+                            className="form-control" 
+                            id="peso"
+                            name="peso" /> 
+                            {errors.peso && <span>This field is required.<br /></span>}
+
                         </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={handleClick}>Close</button>
+                                <button type="submit" className="btn btn-primary">Save changes</button>
+                            </div>   
                         </div>
-                    </form>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary">Save changes</button>
+                    </form>   
                 </div>
                 </div>
             </div>
